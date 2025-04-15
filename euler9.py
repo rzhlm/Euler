b1eb7ff702de44e95115080280e2ffddfabd7f24
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-04-16 01:17:08 +0200

    Euler 9: cleaned up & faster

--- euler9.py
+++ euler9.py
@@ -6,26 +6,22 @@
 # find: a * b * c
 
 # takes 38s to find on laptop (battery)
+# now 0.03s! (removing one degree of freedom!)
 
 from timer import timer
-
-def do():
-    a, b, c = 3, 4, 4
-    while True:
-        if a**2 + b**2 == c**2 and a + b +c == 1_000:
-            print(f"triplet: {a=} {b=} {c=}")
         
-def do2():
+def do():
     for a in range(3, 1_000):
-        for b in range(3,1_000):
-            for c in range(3,1_000):
-                if a**2 + b**2 == c**2 and a+b+c == 1_000:
-                    print(f"{a=} {b=} {c=}")
-                    return
+        for b in range(4, 1_000):
+            #for c in range(3,1_000):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2: #and a+b+c == 1_000:
+                print(f"{a=} {b=} {c=}")
+                return a*b*c
 
 @timer()
 def main() -> None:
-    do2()
+    print(do())
 
 if __name__ == "__main__":
     main()
