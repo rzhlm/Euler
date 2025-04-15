5ec641673f52881a28deae13f510f061c148297e
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-04-15 22:59:22 +0200

    Euler 6: init

---
+++ euler6.py
@@ -0,0 +1,15 @@
+# Euler 6
+#
+# Find the difference between
+# the sum of the squares
+# of the first one hundred natural numbers
+# and the square of their sum.
+#
+
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
