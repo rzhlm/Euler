7163fdffe231edc623be76af04518d313c0cc140
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-04-15 23:10:53 +0200

    Euler 6: solved

--- euler6.py
+++ euler6.py
@@ -6,10 +6,25 @@
 # and the square of their sum.
 #
 
+from timer import timer
 
+def sum_first_n(n: int) -> int:
+    # n * (n+1) / 2
+    return n * (n + 1) // 2
 
+def sum_of_squares(n: int) -> int:
+    output = 0
+    for i in range(1, n + 1):
+        output += i ** 2
+    return output
+
+@timer()
 def main() -> None:
-    pass
+    assert 5050 == sum_first_n(100)
+    square_of_sum = sum_first_n(100) ** 2
+    sum_squares = sum_of_squares(100)
+    diff = abs(sum_squares - square_of_sum)
+    print(diff)
 
 if __name__ == "__main__":
     main()
