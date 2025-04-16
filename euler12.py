f5513fa5688f8751a8480b3dc43661a5b913eb2a
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-04-16 14:25:06 +0200

    Euler 12: init

---
+++ euler12.py
@@ -0,0 +1,12 @@
+# Euler 12
+#
+# which number (which is triangular), is the first with >500 divisors?
+# 
+# triangular(n): adding up naturals including n
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
