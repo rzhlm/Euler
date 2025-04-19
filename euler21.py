c9b4d9c2b52a2a8f1492b4b38e1e65d30d1f467f
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-04-19 20:07:15 +0200

    Euler 21: init

---
+++ euler21.py
@@ -0,0 +1,15 @@
+# Euler 21
+#
+# d(n) = sum of proper divisors of n
+# if d(a) = b
+# and d(b) = a (and a != b)
+# then a,b is 'amicable pair', 'amicable nymbers'
+#
+# find sum of all amicable numbers < 10_000
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
