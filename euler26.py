d3f44cd8be1b5a638f73e85064900dcc63d8947b
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-04-22 14:47:44 +0200

    Euler 26: init

---
+++ euler26.py
@@ -0,0 +1,21 @@
+# Euler 26
+#
+# for 1/d, find the d (< 1_000), which has the longest recurring cycle
+# e.g. 1/6 = 0.166666 (1 recurring)
+# e.g. 1/7 = 0.142857_142857_142857, etc (6 digits)
+#
+
+from timer import timer
+
+
+def solve() -> int:
+    pass
+
+def testing() -> None:
+    pass
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
